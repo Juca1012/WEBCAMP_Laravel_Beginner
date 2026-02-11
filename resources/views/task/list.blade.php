299809190ae813fcefa7e71fa225d5a55e7fa364
kazuki <Juca1012@example.com>
2026-02-11 14:40:20 +0000

    タスクの削除と完了を書いた

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -10,6 +10,16 @@
             @if (session('front.task_register_success') == true)
                     タスクを登録しました！！<br>
             @endif
+            @if (session('front.task_delete_success') == true)
+                    タスクを削除しました！！<br>
+            @endif
+            @if (session('front.task_completed_success') == true)
+                    タスクを完了にしました！！<br>
+            @endif
+            @if (session('front.task_completed_failure') == true)
+                    タスクの完了に失敗しました....<br>
+            @endif
+
             @if ($errors->any())
                 <div>
                 @foreach ($errors->all() as $error)
@@ -28,13 +38,9 @@
                 <button>タスクを登録する</button>
             </form>
     
-        <h1>タスクの一覧(未実装)</h1>
+        <h1>タスクの一覧</h1>
         <a href="./top.html">CSVダウンロード(未実装)</a><br>
         <table border="1">
-        <tr>
-            <th>タスク名
-            <th>期限
-            <th>重要度
         @foreach ($list as $task)
         <tr>
             <td>{{ $task->name }}
@@ -42,7 +48,11 @@
             <td>{{ $task->getPriorityString() }}
             <td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
             <td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a>
-            <td><form action="./top.html"><button>完了</button></form>
+            <td>
+                <form action="{{ route('complete', ['task_id' => $task->id]) }}" method="post">
+                @csrf
+                <button onclick='return confirm("このタスクを「完了」にします。よろしいですか？");' >完了</button>
+            </form>
         @endforeach
         </table>
         <!-- ページネーション -->
@@ -71,5 +81,3 @@
             <a href="/logout">ログアウト</a><br>
          </menu>
 @endsection
-    </body>
-</html>
